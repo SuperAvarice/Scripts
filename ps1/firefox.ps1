4732ddd3f6d0522028737094f73b6b6e016b26ce
James Schwan <james@schwan.us>
2023-11-17 15:18:01 -0600

    ,,,

--- ps1/firefox.ps1
+++ ps1/firefox.ps1
@@ -1,19 +1,19 @@
 $IMAGE="jlesage/firefox"
 $NAME="firefox"
-$VNCPASS= -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 8 | ForEach-Object {[char]$_})
+$DATA="firefox-data"
 
 docker pull $IMAGE
 docker stop $NAME
 docker rm $NAME
 
+docker volume create $DATA
 docker run -d --rm `
     --name=$NAME `
     -p 5800:5800 `
-    --shm-size 2g `
-    -e VNC_PASSWORD=${VNCPASS} `
-    -e DISPLAY_WIDTH=1920 `
-    -e DISPLAY_HEIGHT=1200 `
+    -e DARK_MODE=1 `
+    -e KEEP_APP_RUNNING=1 `
+    -v ${DATA}:/config `
     $IMAGE
 	
-write-host "Go to http://localhost:5800/?password=${VNCPASS}"
+write-host "Go to http://localhost:5800/"
 pause
